f44be91b112c4991ef4586ba6e5325646876987d
Joshua C. Burt <jburt@healthwise.org>
2016-04-23 14:21:57 -0600

    added subdependencies version bump

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,12 @@ maintainer_email 'joshburt@shapeandshare.com'
 license          'Apache License'
 description      'Installs/Configures libsodium'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 issues_url 'https://github.com/joshburt/libsodium/issues'
 source_url 'https://github.com/joshburt/libsodium'
 
 depends 'build-essential'
+depends 'seven_zip'
+depends 'chef_handler'
+depends 'windows'
